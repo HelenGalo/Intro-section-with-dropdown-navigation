00da871a424cdbd2bcdf55e29e8d408e74f8c3e0
HelenGalo <helen.orellana1@ujcv.edu.hn>
2023-01-19 11:51:04 -0600

    modificacion de js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 import '../assets/style/style.css';
-
+//Declaracion de variables
 const lista = document.querySelectorAll(".sub-menu");
 const burger = document.getElementById("burger-menu");
 const close = document.querySelector(".close-menu");
 let nav = document.getElementById("nav");
 const active = document.querySelector(".submenu.active");
 
+
+//funcion para mostrar los sub menus del dropdown
 function activeSubMenu (event) {
   event.preventDefault();
 
-  const actual = event.currentTarget;
+  const actual = event.currentTarget; //
   const subMenu = actual.querySelector(".submenu");
   const arrowImg= actual.querySelector("a .arrow");
 
@@ -25,15 +27,16 @@ function activeSubMenu (event) {
 lista.forEach((menuItem) => {
   menuItem.addEventListener("click", activeSubMenu);
 });
-
+//abre el menu y agrega la clase active
 function openMenu() {
   nav.classList.add("active");
 }
 
 burger.addEventListener("click", openMenu);
-
+//cierre el menu y agrega la clase active
 function closeMenu() {
   nav.classList.remove("active");
 } 
+//Evento de escucha
 
 close.addEventListener("click", closeMenu);
